f2bb5736b43182f55d4c014ab109908df0d9f4ae
Jirrez Matheus <jirrezdex@gmail.com>
2018-12-15 23:37:11 -0200

    MD: i3, polbar, i3dex.sh & sistema.sh

--- config/scripts/sistema.sh
+++ config/scripts/sistema.sh
@@ -3,17 +3,17 @@
 # contato@jirrezdex.com
 # Jirrez Matheus
 
-res=$(echo "-Bloquear;-Reiniciar;-Desligar" | rofi -sep ";" -dmenu -p "Controle do sistema" -bw 0 -separator-style none -location 0 -width 20 -lines 3 -padding 5)
+res=$(echo "-Sair;-Reiniciar;-Desligar" | rofi -sep ";" -dmenu -p "Controle do sistema" -bw 0 -separator-style none -location 0 -width 20 -lines 3 -padding 5)
 if [ ! -z $res ]; then
   case $res in
-    -Bloquear)
-      su -
+    -Sair)
+      i3-msg exit
     ;;
     -Reiniciar)
-      sudo reboot
+      systemctl reboot
     ;;
     -Desligar)
-      sudo poweroff
+      systemctl poweroff
     ;;
   esac
 fi
